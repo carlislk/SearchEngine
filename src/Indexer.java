3b473c94ce9042ea1c7560ab1ce1c9c20286fd3c
Jared Napoli <Jared@Jareds-Air.att.net>
2016-02-22 20:13:13 -0800

    Added tf idf and file output

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,8 +1,11 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
@@ -12,6 +15,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 
 
@@ -19,11 +23,22 @@ import java.util.Iterator;
 public final class Indexer {
 	public static final int FILE_COUNT = 44536;
 	
-	public static void main(String [] args){//static void main(String [] args) {
+	public static void main(String [] args) {
+		long startTime = System.currentTimeMillis();
 		HashMap<String, Integer> term2termid = new HashMap<String, Integer>();
 	    HashMap<Integer, String> termid2term = new HashMap<Integer,String>();
 	    HashMap<String, Integer> doc2docid = new HashMap<String, Integer>();
+	    HashMap<Integer, Integer> docid2wordcount = new HashMap<Integer,Integer>();
+	    HashMap<Integer, HashSet<Integer>> termid2doclist = new HashMap<Integer, HashSet<Integer>>();
 	    HashMap<Integer, ArrayList<Integer>> docid2termlist = new HashMap<Integer, ArrayList<Integer>>();
+	    // Term Frequency
+	    HashMap<Integer, HashMap<Integer,Integer>> termid2tflist = new HashMap<Integer, HashMap<Integer,Integer>>();
+	    HashMap<Integer, HashMap<Integer, Float>> termid2tfidf = new HashMap<Integer, HashMap<Integer,Float>>();
+	    // Inverse Document Frequency
+	    	// write
+	    // Term Frequency Inverse Document Frequency
+	    	// write
+	    
 	    int termid = 1;
 	    int docid = 0;
 	    JSONParser parser = new JSONParser();
@@ -56,20 +71,59 @@ public final class Indexer {
 		    		doc2docid.put(key, docid);
 		    	docid2termlist.put(docid, new ArrayList<Integer>()); // allocate new arraylist for the doc's termlist
 		    	
+		    	int wordcount = 0;
 		    	// iterates over every word in the parsed html
 		    	// adds words to term2termid and docid2termlist when appropriate
 		    	for(String word : Utilities.tokenizeString(Jsoup.parse(input, "UTF-8").text())) {
-		    		if(!term2termid.containsKey(word))
+		    		if(!term2termid.containsKey(word)) { // new word
 		    			term2termid.put(word, termid++);
+		    			int termid_index = term2termid.get(word);
+		    			termid2doclist.put(termid_index, new HashSet<Integer>());
+		    			termid2doclist.get(termid_index).add(docid);
+		    			termid2tflist.put(termid_index, new HashMap<Integer,Integer>());
+		    			termid2tflist.get(termid_index).put(new Integer(docid), new Integer(1));
+		    		}
+		    		else if(!termid2tflist.get(term2termid.get(word)).containsKey(docid)) {
+		    			termid2tflist.get(term2termid.get(word)).put(new Integer(docid), new Integer(1));
+		    			termid2doclist.get(term2termid.get(word)).add(docid);
+		    		}
+		    		else { 
+		    			int termid_index = term2termid.get(word);
+		    			termid2doclist.get(termid_index).add(docid);
+		    			termid2tflist.get(termid_index).put(docid, new Integer(termid2tflist.get(termid_index).get(docid) + 1));
+		    		}
 		    		docid2termlist.get(docid).add(term2termid.get(word));
+		    		wordcount++;
 		    	}
+		    	docid2wordcount.put(docid, wordcount);
 		    	docid++;
+		    	wordcount = 0;
 			}
 			
 		    // this just reverses the term2termid to make the termid2term map
 		    for(String key : term2termid.keySet()) {
 		    	termid2term.put(term2termid.get(key), key);
 		    }
+		    
+		    float tf = 0;
+		    float idf = 0;
+		    float tfidf = 0;
+		    for(Integer term : term2termid.values()) {
+		    	termid2tfidf.put(term, new HashMap<Integer, Float>());
+		    	for(Integer doc : termid2doclist.get(term))
+		    	{
+		    		tf = (float) termid2tflist.get(term).get(doc) / (float) docid2wordcount.get(doc);
+		    		idf = (float) Math.log10((float) FILE_COUNT / (float) termid2doclist.get(term).size());
+		    		//tfidf = (float) (1 + Math.log10(tf) * Math.log10(idf));
+		    		tfidf = tf * idf;
+		    		termid2tfidf.get(term).put(new Integer(doc), new Float(tfidf));
+		    		//System.out.println(tfidf);
+		    	}
+		    }
+		    
+		    long stopTime = System.currentTimeMillis();
+		    long elapsedTime = stopTime - startTime;
+		    System.out.println(elapsedTime);
 		    
 		    // print statements are commented out to save time
 		    /*System.out.print("term2termid: {");
@@ -89,15 +143,36 @@ public final class Indexer {
 		    	System.out.print(key + ": " + doc2docid.get(key) + ", ");
 		    System.out.println("}");*/
 		    
-		    /*
-		    System.out.println("docid2termlist:");
-		    for(int id : docid2termlist.keySet()) {
-		    	System.out.print("{" + id + " [");
-		    	for(int newid : docid2termlist.get(id)) {
-		    		System.out.print(newid + ", " );
-		    	}
-		    	System.out.println("]}");
-			}*/
+		    /*System.out.println("docid2wordcount");
+		    for(int id : docid2wordcount.keySet())
+		    	System.out.println("{" + id + ": " + docid2wordcount.get(id) + "}");*/
+		    
+		    PrintWriter d2tlwriter = new PrintWriter("doc_termList.txt", "UTF-8");
+		    d2tlwriter.println("docid2termlist index");
+		    for(int id : docid2termlist.keySet()) 
+		    	d2tlwriter.println("{" + id + docid2termlist.get(id).toString() + "}");
+		    d2tlwriter.close();
+		    
+		    PrintWriter t2dlwriter = new PrintWriter("term_docList.txt", "UTF-8");
+		    t2dlwriter.println("termid2doclist index");
+		    for(int id : termid2doclist.keySet())
+		    	t2dlwriter.println("{" + id + ": " + termid2doclist.get(id).toString() + "}"); 
+		    t2dlwriter.close();
+		    
+		    PrintWriter t2tfwriter = new PrintWriter("term_docid-tf.txt", "UTF-8");
+		    t2tfwriter.println("termid2tflist index");
+		    for(int id : termid2tflist.keySet())
+		    	t2tfwriter.println("{" + id + ": " + termid2tflist.get(id).toString() + "}");
+		    t2tfwriter.close();
+		    
+		    PrintWriter t2tfidfwriter = new PrintWriter("term_docid-tfidf.txt", "UTF-8");
+		    t2tfidfwriter.println("termid2tfidf index");
+		    for(int id : termid2tfidf.keySet())
+		    	t2tfidfwriter.println("{" + id + ": " + termid2tfidf.get(id).toString() + "}");
+		    t2tfidfwriter.close();
+	    } catch (NullPointerException e)
+	    {
+	    	e.printStackTrace();
 	    } catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
